55b60fbcd0c9bae5e505942409fa83c532840e70
Francisco <fco.olivaress@gmail.com>
2019-06-09 16:20:01 -0400

    Remove unnecessary code

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,101 +4,90 @@ const POKE_API_URL = "https://pokeapi.co/api/v2/"
 import _ from 'lodash'
 
 const actions = {
-    loadPlayerPokemon_old({ commit }, pokemon_name) {
-        console.log("[ACCIONES]", pokemon_name)
+  loadPlayerPokemon_old({ commit }, pokemon_name) {
+    console.log("[ACCIONES]", pokemon_name)
 
-        let pokemon_attacks = []
+    let pokemon_attacks = []
 
-        let player_pokemon = {
-            name: _.capitalize(pokemon_name),
-            hp: 100,
-            attacks: pokemon_attacks,
-            front_sprite: ''
-        }
+    let player_pokemon = {
+      name: _.capitalize(pokemon_name),
+      hp: 100,
+      attacks: pokemon_attacks,
+      front_sprite: ''
+    }
 
-        axios.get(POKE_API_URL + `pokemon/${pokemon_name}/`)
-            .then((response) => {
-                const { moves } = response.data
-                player_pokemon.front_sprite = response.data.sprites['back_default']
-
-                // Selecciono 4 movimientos al azar
-                let sample_movements = _.sampleSize(moves, 4)
-
-                sample_movements.forEach((val) => {
-                    let { url } = val.move
-
-                    // Por cada movimiento se debe ir a buscar su info respectiva (dada en el campo url)
-                    axios.get(url).then((attack_response) => {
-                        // console.log("MOSTRANDO ATTACK RESPONSE")
-                        // console.log(attack_response)
-                        let description = _.find(attack_response.data['flavor_text_entries'], (x) => { return x.language.name === "es" })["flavor_text"]
-                        let name = _.find(attack_response.data['names'], (x) => { return x.language.name === "es" })["name"]
-                        let pp = attack_response.data['pp']
-                        pokemon_attacks.push({ name, description, pp })
-                        // TODO: ACA TAMBIEN SE LE DEBERÍA CALCULAR EL DAÑO AL ATAQUE PROBABLEMENTE
-                    }).catch((error) => {
-                        console.log("[RECORRIDO ATAQUES]", error)
-                    })
-                })
-            }).catch((error) => {
-                console.log("[PEDIR POKEMON]", error)
-            })
-
-
-        console.log(player_pokemon)
-        commit('SET_PLAYER_POKEMON', player_pokemon)
-    },
-
-    async loadPokemon({ commit }, { pokemon_name, target }) {
-        console.log("El target es:", target)
-
-        // Se manda a pedir la info del pokemon
-        const response = await axios.get(POKE_API_URL + `pokemon/${pokemon_name}/`)
-
-        // Se sacan 4 ataques aleatoreos
+    axios.get(POKE_API_URL + `pokemon/${pokemon_name}/`)
+      .then((response) => {
         const { moves } = response.data
+        player_pokemon.front_sprite = response.data.sprites['back_default']
+
+        // Selecciono 4 movimientos al azar
         let sample_movements = _.sampleSize(moves, 4)
 
-        // Se mandan a pedir la info de los 4 ataques en paralelo
-        let array_of_promises = sample_movements.map((val) => {
-            let { url } = val.move
-            return axios.get(url)
+        sample_movements.forEach((val) => {
+          let { url } = val.move
+
+          // Por cada movimiento se debe ir a buscar su info respectiva (dada en el campo url)
+          axios.get(url).then((attack_response) => {
+            // console.log("MOSTRANDO ATTACK RESPONSE")
+            // console.log(attack_response)
+            let description = _.find(attack_response.data['flavor_text_entries'], (x) => { return x.language.name === "es" })["flavor_text"]
+            let name = _.find(attack_response.data['names'], (x) => { return x.language.name === "es" })["name"]
+            let pp = attack_response.data['pp']
+            pokemon_attacks.push({ name, description, pp })
+            // TODO: ACA TAMBIEN SE LE DEBERÍA CALCULAR EL DAÑO AL ATAQUE PROBABLEMENTE
+          }).catch((error) => {
+            console.log("[RECORRIDO ATAQUES]", error)
+          })
         })
+      }).catch((error) => {
+        console.log("[PEDIR POKEMON]", error)
+      })
 
-        // Se espera a que TODOS los ataques hayan retornado
-        let attacks_response = await Promise.all(array_of_promises)
 
-        // Se procesa cada ataque para generar un arreglo de objetos
-        const pokemon_attacks = attacks_response.map((attack) => {
-            let description = _.find(attack.data['flavor_text_entries'], (x) => { return x.language.name === "es" })["flavor_text"]
-            let name = _.find(attack.data['names'], (x) => { return x.language.name === "es" })["name"]
-            let pp = attack.data['pp']
-            // TODO: Ver que daño se le va a poner...
-            return { description, name, pp }
-        })
+    console.log(player_pokemon)
+    commit('SET_PLAYER_POKEMON', player_pokemon)
+  },
 
-        if (target === "player") {
-            const player_pokemon = {
-                name: _.capitalize(pokemon_name),
-                hp: 100, // TODO: Ver que vida se le va a poner
-                attacks: pokemon_attacks,
-                sprite: response.data.sprites['back_default']
-            }
+  async loadPokemon({ commit }, { pokemon_name, target }) {
+    console.log("El target es:", target)
 
-            commit('SET_PLAYER_POKEMON', player_pokemon)
-        }
-        else {
-            const enemy_pokemon = {
-                name: _.capitalize(pokemon_name),
-                hp: 100, // TODO: Ver que vida se le va a poner
-                attacks: pokemon_attacks,
-                sprite: response.data.sprites['front_default']
-            }
+    // Se manda a pedir la info del pokemon
+    const response = await axios.get(POKE_API_URL + `pokemon/${pokemon_name}/`)
 
-            commit('SET_ENEMY_POKEMON', enemy_pokemon)
-        }
+    // Se sacan 4 ataques aleatoreos
+    const { moves } = response.data
+    let sample_movements = _.sampleSize(moves, 4)
 
+    // Se mandan a pedir la info de los 4 ataques en paralelo
+    let array_of_promises = sample_movements.map((val) => {
+      let { url } = val.move
+      return axios.get(url)
+    })
+
+    // Se espera a que TODOS los ataques hayan retornado
+    let attacks_response = await Promise.all(array_of_promises)
+
+    // Se procesa cada ataque para generar un arreglo de objetos
+    const pokemon_attacks = attacks_response.map((attack) => {
+      let description = _.find(attack.data['flavor_text_entries'], (x) => { return x.language.name === "es" })["flavor_text"]
+      let name = _.find(attack.data['names'], (x) => { return x.language.name === "es" })["name"]
+      let pp = attack.data['pp']
+      // TODO: Ver que daño se le va a poner...
+      return { description, name, pp }
+    })
+
+    /* Se procede a mandar a guardar el nuevo pokemon */
+    const pokemon_to_set = {
+      name: _.capitalize(pokemon_name),
+      hp: 100, // TODO: Ver que vida se le va a poner
+      attacks: pokemon_attacks,
+      sprite: target === "player" ? response.data.sprites['back_default'] : response.data.sprites['front_default']
     }
+
+    const ACTION_NAME = target === "player" ? 'SET_PLAYER_POKEMON' : 'SET_ENEMY_POKEMON'
+    commit(ACTION_NAME, pokemon_to_set)
+  }
 }
 
 export default actions
